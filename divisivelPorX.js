4b573ca9d79f818dabf7d2b6a135a7586da8fd9c
iostsicaro <iostsicaro@gmail.com>
2021-03-10 14:58:24 -0300

    Desafio Divisivel Por X, Hackerrank 2021

---
+++ divisivelPorX.js
@@ -0,0 +1,12 @@
+function solucao(numero, x) {
+    // seu código aqui
+    let resto;
+
+    resto = numero % x
+
+    if (resto === 0) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
