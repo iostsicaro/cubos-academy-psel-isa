cfdbe670882e35b816ba3c64eddae93bf2689bcf
iostsicaro <iostsicaro@gmail.com>
2021-03-10 14:58:45 -0300

    Desafio Derrapagem, Hackerrank 2021

---
+++ derrapagem.js
@@ -0,0 +1,8 @@
+function solucao(velocidade, aceleracao) {
+	//seu código aqui  
+  let distancia;
+  
+  distancia = (- velocidade * velocidade) / (2 * aceleracao)
+  
+  console.log(distancia);
+}
