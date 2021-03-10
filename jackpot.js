b0c922c0fdc8496a2af0a5a333983563c32528d2
iostsicaro <iostsicaro@gmail.com>
2021-03-10 15:05:34 -0300

    Desafio Jackpot, Hackerrank 2021

---
+++ jackpot.js
@@ -0,0 +1,8 @@
+function solucao(resultado1, resultado2, resultado3) {
+    // seu código aqui	]
+    if (resultado1 === resultado2 && resultado2 === resultado3) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
